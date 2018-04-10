ff00f43bc56b800cf6c1324ee725cc92c4530bde
longyixuan <m18234089389@163.com>
2018-04-10 17:08:17 +0800

    Site updated: 2018-04-10 17:08:10

--- xzdemo/src/js/wap.js
+++ xzdemo/src/js/wap.js
@@ -4,6 +4,7 @@
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
+            console.log(clientWidth);
             if (clientWidth >= 750) {
                 docEl.style.fontSize = '100px';
             } else {
